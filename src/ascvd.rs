ba2db8760e079cd1c8b7eb92eda20ac1afd133c0
Lars <98775726+lhegstrom@users.noreply.github.com>
2024-01-27 22:01:20 -0500

    refactor of ascvd complete

--- src/ascvd.rs
+++ src/ascvd.rs
@@ -1,5 +1,5 @@
 use crate::covariates::Covariates;
-use crate::utils::validate_input;
+use crate::utils::{common_calculation, validate_input};
 use pyo3::exceptions::PyValueError;
 use pyo3::prelude::*;
 use std::f64;
@@ -29,172 +29,43 @@ pub fn calculate_10_yr_ascvd_risk(
     )?;
 
     let cholesterol_diff = total_cholesterol - hdl_cholesterol;
-    let age_adjusted = (age - 55.0) / 10.0;
+    let adjusted_age = (age - 55.0) / 10.0;
+    let adjusted_age_squared = adjusted_age.powi(2);
 
     match sex.to_lowercase().as_str() {
         "female" => {
             let covariates = Covariates::female_10_yr_ascvd();
-
-            let diabetes_factor = if has_diabetes {
-                covariates.diabetes_factor
-            } else {
-                0.0
-            };
-            let smoker_factor = if current_smoker {
-                covariates.smoker_factor
-            } else {
-                0.0
-            };
-            let htn_meds_factor = if on_htn_meds {
-                covariates.htn_meds_factor
-            } else {
-                0.0
-            };
-            let htn_cholesterol_treatment_factor = if on_cholesterol_meds {
-                covariates.cholesterol_meds_factor
-            } else {
-                0.0
-            };
-            let systolic_bp_adjusted_max = (systolic_bp.max(110.0) - 130.0) / 20.0;
-            let cholesterol_diff_factor =
-                covariates.cholesterol_base_multiplier * cholesterol_diff - 3.5;
-
-            let diabetes_age_factor = if has_diabetes {
-                covariates.diabetes_age_factor * age_adjusted
-            } else {
-                0.0
-            };
-            let smoker_age_factor = if current_smoker {
-                covariates.smoker_age_factor * age_adjusted
-            } else {
-                0.0
-            };
-
-            let calculation = vec![
-                covariates.age_adjustment_factor * age_adjusted,
-                covariates.constant,
-                covariates.total_cholesterol_diff_factor * cholesterol_diff_factor,
-                covariates.hdl_cholesterol_diff_factor
-                    * (covariates.cholesterol_base_multiplier * hdl_cholesterol - 1.3)
-                    / 0.3,
-                covariates.systolic_bp_min_factor * (systolic_bp.min(110.0) - 110.0) / 20.0,
-                covariates.systolic_bp_max_factor * systolic_bp_adjusted_max,
-                diabetes_factor,
-                smoker_factor,
-                covariates.egfr_min_factor * (egfr.min(60.0) - 60.0) / -15.0,
-                covariates.egfr_max_factor * (egfr.max(60.0) - 90.0) / -15.0,
-                htn_meds_factor,
-                htn_cholesterol_treatment_factor,
-                (if on_htn_meds {
-                    covariates.htn_meds_systolic_bp_max_factor * systolic_bp_adjusted_max
-                } else {
-                    0.0
-                }),
-                (if on_cholesterol_meds {
-                    covariates.cholesterol_meds_cholesterol_diff_factor * cholesterol_diff_factor
-                } else {
-                    0.0
-                }),
-                covariates.age_adjustment_cholesterol_diff_factor
-                    * age_adjusted
-                    * cholesterol_diff_factor,
-                covariates.age_adjustment_hdl_cholesterol_diff_factor
-                    * age_adjusted
-                    * (covariates.cholesterol_base_multiplier * hdl_cholesterol - 1.3)
-                    / 0.3,
-                covariates.age_adjustment_systolic_bp_max_factor
-                    * age_adjusted
-                    * systolic_bp_adjusted_max,
-                diabetes_age_factor,
-                smoker_age_factor,
-                covariates.age_min_egfr_factor * age_adjusted * (egfr.min(60.0) - 60.0) / -15.0,
-            ]
-            .iter()
-            .sum();
-
+            let calculation = common_calculation(
+                &covariates,
+                has_diabetes,
+                current_smoker,
+                on_htn_meds,
+                on_cholesterol_meds,
+                systolic_bp,
+                cholesterol_diff,
+                hdl_cholesterol,
+                adjusted_age,
+                adjusted_age_squared,
+                egfr,
+            );
             let risk_score = E.powf(calculation) / (1.0 + E.powf(calculation)) * 100.0;
             Ok(risk_score)
         }
         "male" => {
             let covariates = Covariates::male_10_yr_ascvd();
-
-            let diabetes_factor = if has_diabetes {
-                covariates.diabetes_factor
-            } else {
-                0.0
-            };
-            let smoker_factor = if current_smoker {
-                covariates.smoker_factor
-            } else {
-                0.0
-            };
-            let htn_meds_factor = if on_htn_meds {
-                covariates.htn_meds_factor
-            } else {
-                0.0
-            };
-            let htn_cholesterol_treatment_factor = if on_cholesterol_meds {
-                covariates.cholesterol_meds_factor
-            } else {
-                0.0
-            };
-            let systolic_bp_adjusted_max = (systolic_bp.max(110.0) - 130.0) / 20.0;
-            let cholesterol_diff_factor =
-                covariates.cholesterol_base_multiplier * cholesterol_diff - 3.5;
-
-            let diabetes_age_factor = if has_diabetes {
-                covariates.diabetes_age_factor * age_adjusted
-            } else {
-                0.0
-            };
-            let smoker_age_factor = if current_smoker {
-                covariates.smoker_age_factor * age_adjusted
-            } else {
-                0.0
-            };
-
-            let calculation = vec![
-                covariates.age_adjustment_factor * age_adjusted,
-                covariates.constant,
-                covariates.total_cholesterol_diff_factor * cholesterol_diff_factor,
-                covariates.hdl_cholesterol_diff_factor
-                    * (covariates.cholesterol_base_multiplier * hdl_cholesterol - 1.3)
-                    / 0.3,
-                covariates.systolic_bp_min_factor * (systolic_bp.min(110.0) - 110.0) / 20.0,
-                covariates.systolic_bp_max_factor * systolic_bp_adjusted_max,
-                diabetes_factor,
-                smoker_factor,
-                covariates.egfr_min_factor * (egfr.min(60.0) - 60.0) / -15.0,
-                covariates.egfr_max_factor * (egfr.max(60.0) - 90.0) / -15.0,
-                htn_meds_factor,
-                htn_cholesterol_treatment_factor,
-                (if on_htn_meds {
-                    covariates.htn_meds_systolic_bp_max_factor * systolic_bp_adjusted_max
-                } else {
-                    0.0
-                }),
-                (if on_cholesterol_meds {
-                    covariates.cholesterol_meds_cholesterol_diff_factor * cholesterol_diff_factor
-                } else {
-                    0.0
-                }),
-                covariates.age_adjustment_cholesterol_diff_factor
-                    * age_adjusted
-                    * cholesterol_diff_factor,
-                covariates.age_adjustment_hdl_cholesterol_diff_factor
-                    * age_adjusted
-                    * (covariates.cholesterol_base_multiplier * hdl_cholesterol - 1.3)
-                    / 0.3,
-                covariates.age_adjustment_systolic_bp_max_factor
-                    * age_adjusted
-                    * systolic_bp_adjusted_max,
-                diabetes_age_factor,
-                smoker_age_factor,
-                covariates.age_min_egfr_factor * age_adjusted * (egfr.min(60.0) - 60.0) / -15.0,
-            ]
-            .iter()
-            .sum();
-
+            let calculation = common_calculation(
+                &covariates,
+                has_diabetes,
+                current_smoker,
+                on_htn_meds,
+                on_cholesterol_meds,
+                systolic_bp,
+                cholesterol_diff,
+                hdl_cholesterol,
+                adjusted_age,
+                adjusted_age_squared,
+                egfr,
+            );
             let risk_score = E.powf(calculation) / (1.0 + E.powf(calculation)) * 100.0;
             Ok(risk_score)
         }
@@ -213,7 +84,7 @@ pub fn calculate_30_yr_ascvd_value(
     bmi: f64,
     egfr: f64,
     on_htn_meds: bool,
-    cholesterol_treated: bool,
+    on_cholesterol_meds: bool,
 ) -> Result<f64, String> {
     validate_input(
         age,
@@ -225,162 +96,44 @@ pub fn calculate_30_yr_ascvd_value(
         false,
     )?;
 
-    let cholesterol_difference = total_cholesterol - hdl_cholesterol;
-    let age_factor = (age - 55.0) / 10.0;
-    let age_squared = age_factor.powi(2);
+    let cholesterol_diff = total_cholesterol - hdl_cholesterol;
+    let adjusted_age = (age - 55.0) / 10.0;
+    let adjusted_age_squared = adjusted_age.powi(2);
 
     match sex.to_lowercase().as_str() {
         "female" => {
             let covariates = Covariates::female_30_yr_ascvd();
-
-            let calculation = vec![
-                covariates.age_adjustment_factor * age_factor,
-                covariates.constant,
-                covariates.age_squared_factor * age_squared,
-                covariates.total_cholesterol_diff_factor
-                    * (covariates.cholesterol_base_multiplier * cholesterol_difference - 3.5),
-                covariates.hdl_cholesterol_diff_factor
-                    * (covariates.cholesterol_base_multiplier * hdl_cholesterol - 1.3)
-                    / 0.3,
-                covariates.systolic_bp_min_factor * (systolic_bp.min(110.0) - 110.0) / 20.0,
-                covariates.systolic_bp_max_factor * (systolic_bp.max(110.0) - 130.0) / 20.0,
-                if has_diabetes {
-                    covariates.diabetes_factor
-                } else {
-                    0.0
-                },
-                if current_smoker {
-                    covariates.smoker_factor
-                } else {
-                    0.0
-                },
-                covariates.egfr_min_factor * (egfr.min(60.0) - 60.0) / -15.0,
-                covariates.egfr_max_factor * (egfr.max(60.0) - 90.0) / -15.0,
-                if on_htn_meds {
-                    covariates.htn_meds_factor
-                } else {
-                    0.0
-                },
-                if cholesterol_treated {
-                    covariates.cholesterol_meds_factor
-                } else {
-                    0.0
-                },
-                if on_htn_meds {
-                    covariates.htn_meds_systolic_bp_max_factor * (systolic_bp.max(110.0) - 130.0)
-                        / 20.0
-                } else {
-                    0.0
-                },
-                if cholesterol_treated {
-                    covariates.cholesterol_meds_cholesterol_diff_factor
-                        * (covariates.cholesterol_base_multiplier * cholesterol_difference - 3.5)
-                } else {
-                    0.0
-                },
-                covariates.age_adjustment_cholesterol_diff_factor
-                    * age_factor
-                    * (covariates.cholesterol_base_multiplier * cholesterol_difference - 3.5),
-                covariates.age_adjustment_hdl_cholesterol_diff_factor
-                    * age_factor
-                    * (covariates.cholesterol_base_multiplier * hdl_cholesterol - 1.3)
-                    / 0.3,
-                covariates.age_adjustment_systolic_bp_max_factor
-                    * age_factor
-                    * (systolic_bp.max(110.0) - 130.0)
-                    / 20.0,
-                if has_diabetes {
-                    covariates.diabetes_age_factor * age_factor
-                } else {
-                    0.0
-                },
-                if current_smoker {
-                    covariates.smoker_age_factor * age_factor
-                } else {
-                    0.0
-                },
-                covariates.age_min_egfr_factor * age_factor * (egfr.min(60.0) - 60.0) / -15.0,
-            ]
-            .iter()
-            .sum();
-
+            let calculation = common_calculation(
+                &covariates,
+                has_diabetes,
+                current_smoker,
+                on_htn_meds,
+                on_cholesterol_meds,
+                systolic_bp,
+                cholesterol_diff,
+                hdl_cholesterol,
+                adjusted_age,
+                adjusted_age_squared,
+                egfr,
+            );
             let risk_score = E.powf(calculation) / (1.0 + E.powf(calculation)) * 100.0;
             Ok(risk_score)
         }
         "male" => {
             let covariates = Covariates::male_30_yr_ascvd();
-
-            let calculation = vec![
-                covariates.age_adjustment_factor * age_factor,
-                covariates.constant,
-                covariates.age_squared_factor * age_squared,
-                covariates.total_cholesterol_diff_factor
-                    * (covariates.cholesterol_base_multiplier * cholesterol_difference - 3.5),
-                covariates.hdl_cholesterol_diff_factor
-                    * (covariates.cholesterol_base_multiplier * hdl_cholesterol - 1.3)
-                    / 0.3,
-                covariates.systolic_bp_min_factor * (systolic_bp.min(110.0) - 110.0) / 20.0,
-                covariates.systolic_bp_max_factor * (systolic_bp.max(110.0) - 130.0) / 20.0,
-                if has_diabetes {
-                    covariates.diabetes_factor
-                } else {
-                    0.0
-                },
-                if current_smoker {
-                    covariates.smoker_factor
-                } else {
-                    0.0
-                },
-                covariates.egfr_min_factor * (egfr.min(60.0) - 60.0) / -15.0,
-                covariates.egfr_max_factor * (egfr.max(60.0) - 90.0) / -15.0,
-                if on_htn_meds {
-                    covariates.htn_meds_factor
-                } else {
-                    0.0
-                },
-                if cholesterol_treated {
-                    covariates.cholesterol_meds_factor
-                } else {
-                    0.0
-                },
-                if on_htn_meds {
-                    covariates.htn_meds_systolic_bp_max_factor * (systolic_bp.max(110.0) - 130.0)
-                        / 20.0
-                } else {
-                    0.0
-                },
-                if cholesterol_treated {
-                    covariates.cholesterol_meds_cholesterol_diff_factor
-                        * (covariates.cholesterol_base_multiplier * cholesterol_difference - 3.5)
-                } else {
-                    0.0
-                },
-                covariates.age_adjustment_cholesterol_diff_factor
-                    * age_factor
-                    * (covariates.cholesterol_base_multiplier * cholesterol_difference - 3.5),
-                covariates.age_adjustment_hdl_cholesterol_diff_factor
-                    * age_factor
-                    * (covariates.cholesterol_base_multiplier * hdl_cholesterol - 1.3)
-                    / 0.3,
-                covariates.age_adjustment_systolic_bp_max_factor
-                    * age_factor
-                    * (systolic_bp.max(110.0) - 130.0)
-                    / 20.0,
-                if has_diabetes {
-                    covariates.diabetes_age_factor * age_factor
-                } else {
-                    0.0
-                },
-                if current_smoker {
-                    covariates.smoker_age_factor * age_factor
-                } else {
-                    0.0
-                },
-                covariates.age_min_egfr_factor * age_factor * (egfr.min(60.0) - 60.0) / -15.0,
-            ]
-            .iter()
-            .sum();
-
+            let calculation = common_calculation(
+                &covariates,
+                has_diabetes,
+                current_smoker,
+                on_htn_meds,
+                on_cholesterol_meds,
+                systolic_bp,
+                cholesterol_diff,
+                hdl_cholesterol,
+                adjusted_age,
+                adjusted_age_squared,
+                egfr,
+            );
             let risk_score = E.powf(calculation) / (1.0 + E.powf(calculation)) * 100.0;
             Ok(risk_score)
         }
--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,4 @@
+use crate::covariates::Covariates;
 use std::f64;
 
 pub fn validate_input(
@@ -9,8 +10,11 @@ pub fn validate_input(
     egfr: f64,
     ten_year: bool,
 ) -> Result<(), String> {
-
-    if ten_year { validate_10_yr_age(&age)? } else { validate_30_yr_age(&age)? }
+    if ten_year {
+        validate_10_yr_age(&age)?
+    } else {
+        validate_30_yr_age(&age)?
+    }
 
     if !(130.0..=320.0).contains(&total_cholesterol) {
         return Err("Total cholesterol must be between 130 and 320".to_string());
@@ -42,4 +46,95 @@ fn validate_30_yr_age(&age: &f64) -> Result<(), String> {
         return Err("Age must be between 30 and 59".to_string());
     }
     Ok(())
+}
+
+pub fn common_calculation(
+    covariates: &Covariates,
+    has_diabetes: bool,
+    current_smoker: bool,
+    on_htn_meds: bool,
+    on_cholesterol_meds: bool,
+    systolic_bp: f64,
+    cholesterol_diff: f64,
+    hdl_cholesterol: f64,
+    adjusted_age: f64,
+    adjusted_age_squared: f64,
+    egfr: f64,
+) -> f64 {
+    let diabetes_factor = if has_diabetes {
+        covariates.diabetes_factor
+    } else {
+        0.0
+    };
+    let smoker_factor = if current_smoker {
+        covariates.smoker_factor
+    } else {
+        0.0
+    };
+    let htn_meds_factor = if on_htn_meds {
+        covariates.htn_meds_factor
+    } else {
+        0.0
+    };
+    let htn_cholesterol_treatment_factor = if on_cholesterol_meds {
+        covariates.cholesterol_meds_factor
+    } else {
+        0.0
+    };
+    let systolic_bp_adjusted_max = (systolic_bp.max(110.0) - 130.0) / 20.0;
+    let systolic_bp_adjusted_min = (systolic_bp.min(110.0) - 110.0) / 20.0;
+    let cholesterol_diff_factor = covariates.cholesterol_base_multiplier * cholesterol_diff - 3.5;
+    let hdl_cholesterol_diff_factor =
+        (covariates.cholesterol_base_multiplier * hdl_cholesterol - 1.3) / 0.3;
+
+    let diabetes_age_factor = if has_diabetes {
+        covariates.diabetes_age_factor * adjusted_age
+    } else {
+        0.0
+    };
+    let smoker_age_factor = if current_smoker {
+        covariates.smoker_age_factor * adjusted_age
+    } else {
+        0.0
+    };
+
+    let egfr_adjusted_min = (egfr.min(60.0) - 60.0) / -15.0;
+    let egfr_adjusted_max = (egfr.max(60.0) - 90.0) / -15.0;
+
+    vec![
+        covariates.age_adjustment_factor * adjusted_age,
+        covariates.age_squared_factor * adjusted_age_squared,
+        covariates.constant,
+        covariates.total_cholesterol_diff_factor * cholesterol_diff_factor,
+        covariates.hdl_cholesterol_diff_factor * hdl_cholesterol_diff_factor,
+        covariates.systolic_bp_min_factor * systolic_bp_adjusted_min,
+        covariates.systolic_bp_max_factor * systolic_bp_adjusted_max,
+        diabetes_factor,
+        smoker_factor,
+        covariates.egfr_min_factor * egfr_adjusted_min,
+        covariates.egfr_max_factor * egfr_adjusted_max,
+        htn_meds_factor,
+        htn_cholesterol_treatment_factor,
+        (if on_htn_meds {
+            covariates.htn_meds_systolic_bp_max_factor * systolic_bp_adjusted_max
+        } else {
+            0.0
+        }),
+        (if on_cholesterol_meds {
+            covariates.cholesterol_meds_cholesterol_diff_factor * cholesterol_diff_factor
+        } else {
+            0.0
+        }),
+        covariates.age_adjustment_cholesterol_diff_factor * adjusted_age * cholesterol_diff_factor,
+        covariates.age_adjustment_hdl_cholesterol_diff_factor
+            * adjusted_age
+            * (covariates.cholesterol_base_multiplier * hdl_cholesterol - 1.3)
+            / 0.3,
+        covariates.age_adjustment_systolic_bp_max_factor * adjusted_age * systolic_bp_adjusted_max,
+        diabetes_age_factor,
+        smoker_age_factor,
+        covariates.age_min_egfr_factor * adjusted_age * egfr_adjusted_min,
+    ]
+    .iter()
+    .sum()
 }
